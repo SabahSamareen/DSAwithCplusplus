da39d27507e698181e49bd6a2a43dfe5b21ae243
Sabah <67190764+SabahSamareen@users.noreply.github.com>
2025-07-18 17:52:26 +0530

    Update 1.1Darray.cpp

--- 1.1Darray.cpp
+++ 1.1Darray.cpp
@@ -9,3 +9,16 @@ using namespace std;
      for(int i=0;i<n;i++)
      cout<<a[i]<<" ";
  }
+
+
+/*
+int a[100]={9}; then uninit values equal 9
+printarrr(int a[])
+...
+int a[]={1,2,3,4,5}
+printarr(a);
+
+gives unexpected results
+it is best to pass size of array along with it
+this is because the value passed is the address of the0th element
+*/
